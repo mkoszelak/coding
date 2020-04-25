cbeb5796d7e79e8ab455376651c525fa3abd5a8a
Michał <m.koszelak@gmail.com>
2020-04-25 13:57:27 +0200

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,5 @@
+print('Witaj świecie!') #Wyświetla napis "Witaj świecie"
+imie = input('Jak masz na imię? \n') #Pyta użytkownika o jego imię
+print('Miło Cię poznać, '+ imie + '. \nLiczba znaków w Twoim imieniu wynosi: ' + str(len(imie))) #Podaje imię użytkownika oraz oblicza ilość znaków w jego imieniu
+wiek = input("Ile masz lat? \n") #Pyta użytkownika o wiek
+print('Twój wiek to: ' + wiek + ' lat. \nZa rok będziesz mieć ' + str(int(wiek)+1) + ' lat.') #Podaje wiek użytkownika i informuje ile lat będzie miał za rok
